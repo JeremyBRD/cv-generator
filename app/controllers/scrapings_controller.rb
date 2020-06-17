dc4ea79b38756b819d98514ecd4490049c8acb0a
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-06-17 16:31:57 +0200

    few changes

--- app/controllers/scrapings_controller.rb
+++ app/controllers/scrapings_controller.rb
@@ -1,2 +1,14 @@
-class ScrapingsController < ApplicationController
+# class ScrapingsController < ApplicationController
+require 'nokogiri'
+require 'httparty'
+require 'byebug'
+
+def scraper
+  url = "https://kitt.lewagon.com/alumni/JeremyBRD"
+  unpased_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unspased_page)
+  byebug
 end
+
+scraper
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  has_many :scraping
 end
